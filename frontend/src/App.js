63dee4998db62a808a4df5fef324c5eb8b94760a
smilejkLee <smileagain7m@gmail.com>
2018-12-14 09:12:50 +0900

    installing webpack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="style.App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
