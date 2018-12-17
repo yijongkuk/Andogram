7d7172fd101203eb56979eb85eeaec9a86f87f4d
smilejkLee <smileagain7m@gmail.com>
2018-12-18 06:20:50 +0900

    Redux Setting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "./redux/configureStore";
+import I18n from "redux-i18n";
+import App from "components/App";
+import { translations } from "./translations";
 
 
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <I18n translations={translations} initiaLang="en" fallbackLang="en">
+        <App />
+      </I18n>
+    </ConnectedRouter>
+  </Provider>,
+document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
